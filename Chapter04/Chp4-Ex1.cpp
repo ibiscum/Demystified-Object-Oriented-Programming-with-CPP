bd033bef6013c223c3ea19b8cbb140525b6553ea
ibiscum <ulf.luecking@gmail.com>
2021-08-24 00:31:39 +0200

    removed typos.

--- Chapter04/Chp4-Ex1.cpp
+++ Chapter04/Chp4-Ex1.cpp
@@ -8,30 +8,30 @@ using std::endl;
 
 int main()
 {
-   int x = 10;
-   int *p = new int;
+	int x = 10;
+	int* p = new int;
 
-   *p = 20;
-  
-   // References must be initialized to the object they are referencing
-   // and may never be assigned another object to reference.
-   // The reference and the referenced object must be the same type. 
-   int &refInt1 = x;  // reference to an integer
-   int &refInt2 = *p; // also, a reference to an integer
+	*p = 20;
 
-   cout << x << " " << *p << " " << refInt1 << " " << refInt2 << endl;
+	// References must be initialized to the object they are referencing
+	// and may never be assigned another object to reference.
+	// The reference and the referenced object must be the same type.
+	int& refInt1 = x;  // reference to an integer
+	int& refInt2 = *p; // also, a reference to an integer
 
-   x++;      // updates x and refInt1
-   (*p)++;   // updates *p and refInt2 
+	cout << x << " " << *p << " " << refInt1 << " " << refInt2 << endl;
 
-   cout << x << " " << *p << " " << refInt1 << " " << refInt2 << endl;
+	x++;      // updates x and refInt1
+	(*p)++;   // updates *p and refInt2
 
-   refInt1++;    // updates refInt1 and x
-   refInt2++;    // updates refInt2 and *p
+	cout << x << " " << *p << " " << refInt1 << " " << refInt2 << endl;
 
-   cout << x << " " << *p << " " << refInt1 << " " << refInt2 << endl;
+	refInt1++;    // updates refInt1 and x
+	refInt2++;    // updates refInt2 and *p
 
-   delete p;
+	cout << x << " " << *p << " " << refInt1 << " " << refInt2 << endl;
 
-   return 0;
+	delete p;
+
+	return 0;
 }
